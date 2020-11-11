e9078398ef01951a434d1cb48cec8d30d4c30699
MOTHMAN-404 <shawnmcdermott17@gmail.com>
2020-11-10 21:32:46 -0500

    program outline

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,1 +1,25 @@
 # TIC TAC TOE
+
+
+# 1 -  Create Dictionary for tic tac toe board
+
+# 2 - Print the empty board
+
+# 3 - Create while loop to get inputs
+
+    # 3.a - Keep Track of which player's turn it is
+
+
+# 4 - Update the dictionary based on input
+
+
+# 5 - Update and print the Board based on dictionary
+
+
+# 6 - Check for win conditions
+#       ( TOP-L + TOP-M + TOP-R has value of X)
+#       etc...
+
+# 7 - If win Condition is met end game 
+
+#      Else return to step 3
